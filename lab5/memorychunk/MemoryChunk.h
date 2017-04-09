030d3dd88995c92ca637d948aa60ca3eae2d1ef7
Janek <ignatowicz@student.agh.edu.pl>
2017-04-09 21:09:39 +0200

    lab5

---
+++ lab5/memorychunk/MemoryChunk.h
@@ -0,0 +1,29 @@
+//
+// Created by janek on 09.04.17.
+//
+
+#ifndef JIMP_EXERCISES_MEMORYCHUNK_H
+#define JIMP_EXERCISES_MEMORYCHUNK_H
+
+
+
+namespace memory {
+
+    class MemoryChunk {
+
+    public:
+        MemoryChunk(std::size_t sz);
+
+        ~MemoryChunk();
+
+        int8_t *MemoryAt(size_t offset) const;
+
+        size_t ChunkSize() const;
+
+    private:
+        int8_t *ptr_;
+
+    };
+}
+
+#endif //JIMP_EXERCISES_MEMORYCHUNK_H
---
+++ lab5/textpool/main.cpp
@@ -0,0 +1,22 @@
+//
+// Created by janek on 09.04.17.
+//
+
+#include <iostream>
+#include "TextPool.h"
+
+using namespace pool;
+using namespace std;
+
+int main() {
+    //Inicjalizacja wstępna puli za pomocą listy inicjalizacyjnej
+    TextPool pool {"abc", "efg", "hij", "klmn"};
+
+    cout << pool.StoredStringCount() << endl;
+    pool.Intern("abc");
+
+    cout << pool.StoredStringCount() << endl;
+    pool.Intern("bysiewicz");
+
+    cout << pool.StoredStringCount() << endl;
+}
